08cf5274ee0a512d9058f9ce1e20d4fb644e8b48
jmfp <therealjmfp@gmail.com>
2024-04-22 16:01:18 -0400

    added sass support

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ const NavBar = () =>{
     return(
         
     <>
-    <div className="w-full h-20 bg-emerald-800 sticky top-0">
+    <div className="w-full h-20 sticky top-0">
       <div className="container mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
           <ul className="hidden md:flex gap-x-6 text-white">
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./sass/pages/Home.module.scss"
 
 export default function Home() {
   return (
     <div>
       <Image
+        className={styles.header}
         src='https://images.unsplash.com/photo-1547916721-7469af15e2a3?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         width={1920}
         height={500}
