b001c6ee7fecc9efbe068cd341444a01809153d3
jmfp <therealjmfp@gmail.com>
2024-05-15 21:42:18 -0400

    testing for vercel deployment

--- app/api/postlist/route.ts
+++ app/api/postlist/route.ts
@@ -3,10 +3,15 @@ import Post from "@/app/models/Post";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    await dbConnect()
-    console.log(`database connected`)
-    const posts = await Post.find({})
-    //return allPosts
-    //return JSON.stringify(posts)
-    return NextResponse.json(posts, {status: 201})  
+    try {
+        await dbConnect()
+        console.log(`database connected`)
+        const posts = await Post.find({})
+        //return allPosts
+        //return JSON.stringify(posts)
+        return NextResponse.json(posts, {status: 201})  
+    } catch (error: any) {
+        return NextResponse.json(error.message)
+    }
+    
 }
