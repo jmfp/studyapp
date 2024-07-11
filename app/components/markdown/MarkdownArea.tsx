4518a93f5068cf545266a5114b36c86d872f255c
jmfp <therealjmfp@gmail.com>
2024-07-10 21:52:00 -0400

    auth

--- app/components/markdown/MarkdownArea.tsx
+++ app/components/markdown/MarkdownArea.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils"
 import 'highlight.js/styles/base16/pop.css'
 import { PiTerminalThin } from "react-icons/pi"
 import Copybutton from "../button/Button"
+import { icons } from "@/app/lib/languageicons"
 
 export default function MarkdownArea({content, children, className}:{content:string, children?:any, className?:string}) {
   return (
@@ -20,10 +21,15 @@ export default function MarkdownArea({content, children, className}:{content:str
             const match = /language-(\w+)/.exec(className || "")
             const id = (Math.floor(Math.random() * 100) + 1).toString()
             if(match?.length){
+                let Icon = PiTerminalThin
+                const isMatch = icons.hasOwnProperty(match[1])
+                if(isMatch){
+                    Icon = icons[match[1] as keyof typeof icons]
+                }
                 return <div className="bg-gradient-to-b text-gray-300 border rounded-md">
                     <div className='px-5 py-2 border-b flex items-center justify-between'>
                         <div className="flex items-center gap-2">
-                            <PiTerminalThin/>
+                            <Icon/>
                             <span>{
                                 //@ts-ignore
                                 node?.data?.meta
---
+++ app/lib/languageicons/index.ts
@@ -0,0 +1,10 @@
+import {SiCplusplus, SiCsharp, SiJavascript, SiPython, SiReact, SiTypescript} from 'react-icons/si'
+export const icons = {
+    js:SiJavascript,
+    react:SiReact,
+    py:SiPython,
+    cs:SiCsharp,
+    cpp:SiCplusplus,
+    ts:SiTypescript,
+    tsx:SiTypescript
+}
