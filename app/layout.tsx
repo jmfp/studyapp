ebd551f5db9466824d5730bc56db08e492934296
jmfp <therealjmfp@gmail.com>
2024-08-09 15:22:13 -0400

    deleted stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,15 @@ import { Inter, Nunito } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation/navbar/Navbar"
 import {ThemeProvider} from './components/theme-provider'
-import { Providers } from "../app/blog/[slug]/providers";
+//import { Providers } from "../app/blog/[slug]/providers";
 
 
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Dungeons and Dragons",
-  description: "Online tabletop rpg tools",
+  title: "Social Media",
+  description: "Social Media",
 };
 
 export default function RootLayout({
@@ -30,13 +30,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Providers>
           <Navigation/>
           {children}
           <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js" async></script>
           <script async>hljs.highlightAll();</script>
           <script async>hljs.highlightOnLoad();</script>
-          </Providers>
         </ThemeProvider>
       </body>
     </html>
